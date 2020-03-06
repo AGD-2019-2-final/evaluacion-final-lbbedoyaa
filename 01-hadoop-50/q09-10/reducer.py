713a766e7b90aa4f6e64cae76c8ccaa65664d966
luis <lbbedoyaa@unal.edu.co>
2020-03-06 18:01:31 -0500

    Update reducer.py

--- 01-hadoop-50/q09-10/reducer.py
+++ 01-hadoop-50/q09-10/reducer.py
@@ -2,3 +2,15 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == '__main__':
+    counter=0
+    for line in sys.stdin:
+        
+        if counter<6:
+          val, key, fecha = line.split("\t")
+          fecha=fecha.strip()
+          val = int(val)
+          sys.stdout.write("{}\t{}\t{}\n".format(key, fecha, val))
+          counter=counter+1
+        else:
+          break
