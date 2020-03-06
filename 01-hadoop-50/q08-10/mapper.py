64d13f79b13dc314d71717bfb623c164eaeb3642
luis <lbbedoyaa@unal.edu.co>
2020-03-06 18:01:13 -0500

    Update mapper.py

--- 01-hadoop-50/q08-10/mapper.py
+++ 01-hadoop-50/q08-10/mapper.py
@@ -3,3 +3,10 @@ import sys
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 
+if __name__ == "__main__":
+
+       for line in sys.stdin:
+         c1,c2,c3 = line.split("   ")
+         c3=c3.strip() ##Me quita los caracteres adiconales
+                   
+         sys.stdout.write("{},{}\n".format(c1,c3))
