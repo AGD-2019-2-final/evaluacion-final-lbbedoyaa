1a246a8593d252f396f2e8bf5b2776c32cebd3c4
luis <lbbedoyaa@unal.edu.co>
2020-03-06 18:00:55 -0500

    Update reducer.py

--- 01-hadoop-50/q06-10/reducer.py
+++ 01-hadoop-50/q06-10/reducer.py
@@ -2,3 +2,39 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    curkey = None
+    curmax = 0
+    curmin=100000
+
+    ##
+    ## cada linea de texto recibida es una
+    ## entrada clave \tabulador valor
+    ##
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+
+        if key == curkey:
+            if val>curmax:
+              curmax=val 
+            if val<curmin:
+              curmin=val
+        else:
+            ##
+            ## Se cambio de clave. Se reinicia el
+            ## acumulador.
+            ##
+            if curkey is not None:
+                ##
+                ## una vez se han reducido todos los elementos
+                ## con la misma clave se imprime el resultado en
+                ## el flujo de salida
+                ##
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, curmax, curmin))
+            curkey = key
+            curmax=val
+            curmin=val
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, curmax, curmin))
