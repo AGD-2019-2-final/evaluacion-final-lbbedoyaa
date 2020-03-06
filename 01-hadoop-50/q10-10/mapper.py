7c4264373cc48d9589533d3e3b04eda806fd4d8e
luis <lbbedoyaa@unal.edu.co>
2020-03-06 18:01:34 -0500

    Update mapper.py

--- 01-hadoop-50/q10-10/mapper.py
+++ 01-hadoop-50/q10-10/mapper.py
@@ -2,4 +2,13 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-        
+if __name__ == "__main__":
+
+      for line in sys.stdin:
+        key, valores= line.split("\t")
+        valores=valores.strip()
+        for i in range(0,2-len(key)):
+            key="0"+key
+        for val in valores.split(","):
+          
+          sys.stdout.write("{}\t{}\n".format(val, key))        
