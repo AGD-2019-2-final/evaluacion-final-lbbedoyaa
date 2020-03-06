f9c9d3680ddef4a840ce6a2e5b1ffe5d8cc246c8
luis <lbbedoyaa@unal.edu.co>
2020-03-06 18:00:18 -0500

    Update reducer.py

--- 01-hadoop-50/q03-10/reducer.py
+++ 01-hadoop-50/q03-10/reducer.py
@@ -2,3 +2,11 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    for line in sys.stdin:
+        val, key = line.split("\t")
+        key = key.strip()
+        
+        sys.stdout.write("{},{}\n".format(key,val))
+        
